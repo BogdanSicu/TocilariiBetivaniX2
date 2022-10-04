e028ae1a0c12538115dc0925c060d52be6f8c32e
unknown <b0gdan.munt3@gmail.com>
2022-10-04 22:12:02 +0300

    Start

---
+++ back/second-sell/src/main/java/tocilariibetivani/secondsell/SecondSellApplication.java
@@ -0,0 +1,13 @@
+package tocilariibetivani.secondsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSellApplication.class, args);
+	}
+
+}
